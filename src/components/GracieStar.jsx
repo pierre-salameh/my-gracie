5b982d0c82ec9570ef5deea5f050b0b50da8a3c5
Pierre-Salameh <pierresalam6@gmail.com>
2026-06-08 01:46:11 +0300

    Fix mobile star positionnnnnnnn

--- src/components/GracieStar.jsx
+++ src/components/GracieStar.jsx
@@ -1,32 +1,26 @@
 import { motion } from "framer-motion";
 
 export default function GracieStar() {
-
   return (
-
     <motion.div
       className="gracie-star"
       animate={{
         scale: [1, 1.08, 1]
       }}
       transition={{
-        duration: 4,
-        repeat: Infinity,
-        ease: "easeInOut"
-      }}
-      style={{
-        willChange: "transform"
+        duration: 3,
+        repeat: Infinity
       }}
     >
-
-      ✦
+      <img
+        src="/star.png"
+        alt="Gracie"
+        className="gracie-star-image"
+      />
 
       <div className="star-name">
         Gracie
       </div>
-
     </motion.div>
-
   );
-
 }
